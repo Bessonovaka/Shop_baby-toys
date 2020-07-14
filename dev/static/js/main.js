3e6285e061a7016a0cdb4c30280c20b2d2c4c424
bessonovaka <bessonova.en@mail.ru>
2020-07-14 10:14:34 +0300

    nav-menu, subnav-menu

---
+++ dev/static/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    svg4everybody({});
+
+    let mainSubnavHover = () => {
+        $('.main-subnav__item').hover(
+            function () {
+                let parentList = $(this).closest('.main-subnav__list');
+                if ($(this).children('.main-subnav__list').length) {
+                    let catNavHeight = $(this).children('.main-subnav__list').outerHeight();
+                    if (parentList.outerHeight() < catNavHeight) {
+                        parentList.css('height', catNavHeight);
+                    }
+                    parentList.css('width', '720');
+                }
+            }, function () {
+                let parentList = $(this).closest('.main-subnav__list');
+                parentList.css('height', 'auto');
+                parentList.css('width', 'auto');
+            }
+        )
+    };
+
+    mainSubnavHover();
+
+});
